6bed1dcfbe95889ff374fec3c29e4fd26cd12c8b
Justin Burgener <justinburgener@gmail.com>
2023-12-23 03:27:21 -0500

    california cohort w csv exports

--- synthea_setup.sh
+++ synthea_setup.sh
@@ -3,4 +3,4 @@
 # Script used to generate example dataset from Synthea. Details at https://github.com/synthetichealth/synthea/wiki/Basic-Setup-and-Running
 # Requires Java 11 or newer
 
-java -jar bin/synthea-with-dependencies.jar -s 4121 -c synthea_configuration.txt
+java -jar bin/synthea-with-dependencies.jar -s 4121 -p 500 California
